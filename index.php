e60880fce4aa9a073151ca0291b3630887bdcd8d
Goncalo Taleigo <goncalotaleigo@gmail.com>
2025-01-02 21:52:07 +0000

    Start project, created login page structure

---
+++ index.php
@@ -0,0 +1,36 @@
+<!-- Login page -->
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="admin/css/login.css">
+</head>
+
+<body>
+    <div class="login-page">
+        <h2>Cyber Cafe Management System</h2>
+        <div class="login-container">
+
+            <form action="" method="post">
+                <div class="form-element">
+                    <label for="">User Name</label>
+                    <input type="text" name="username" placeholder="User Name">
+                </div>
+                <div class="form-element">
+                    <label for="">Password</label>
+                    <input type="text" name="password" placeholder="Password">
+                </div>
+                <a href="" class="forgot-password">Forgot Password?</a>
+                <input type="submit" value="SIGN IN" class="sign-in">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
